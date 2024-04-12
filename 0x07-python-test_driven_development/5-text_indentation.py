4d4e588daa826b7ebd960167627d26ba72fb7b12
StalloneSimon <Stallonendungu@gmail.com>
2024-04-12 10:38:32 +0000

    this is a fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,14 +2,26 @@
 """
 We are printing a text with 2 new lines after each '.', '?', ':'
 """
+
 def text_indentation(text):
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-    newText = ""
-    for i in range(len(text)):
-        if not i == 0 and text[i] == " " and text[i - 1] in ".?:":
-            continue
-        newText += text[i]
-        if text[i] in ".?:":
-            newText += "\n\n"
-    print(newText, end="")
+    """ insert doble jump line after . : or ? """
+    str_error = "text must be a string"
+    new_text = ""
+    flag = False
+    if type(text) is not str:
+        raise TypeError(str_error)
+    new_text = text.replace(". ", ".")
+    new_text = new_text.replace(": ", ":")
+    new_text = new_text.replace("? ", "?")
+    for char in new_text:
+        if char in [".", "?", ":"]:
+            print(char)
+            print()
+            flag = True
+        else:
+            if flag is False:
+                print(char, end="")
+            else:
+                if char != " ":
+                    print(char, end="")
+                    flag = False
